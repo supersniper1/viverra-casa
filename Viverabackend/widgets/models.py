2fa90090c54dae95a536bca702f4f0b7402de9aa
Sovraska <simon.novikov@gmail.com>
2023-03-14 02:30:44 +0300

    add Widgets model

--- Viverabackend/widgets/models.py
+++ Viverabackend/widgets/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Widgets(models.Model):
+    widget_tag = models.CharField(max_length=100)
+    widget_x = models.IntegerField()
+    widget_y = models.IntegerField()
+    widget_size_x = models.IntegerField()
+    widget_size_y = models.IntegerField()
+
+
+class WidgetsDiscord(Widgets):
+    tracked_server = models.CharField(max_length=100)
+
+
+class WidgetsTwitter(Widgets):
+    tracked_name = models.CharField(max_length=100)
+
+
+class WidgetsNote(Widgets):
+    text = models.TextField()
