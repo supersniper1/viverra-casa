cce27547e68f7ab0b2768c93faf2098772c8d00b
supersniper1 <denchikden1721@gmail.com>
2023-03-14 20:37:51 +0500

    redux typed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import React, {FunctionComponent} from "react";
-import {Square} from "./components/square/square.component";
-import {Buttons} from "./components/buttons/buttons.component";
+import { Square } from "./components/square/square.component";
+import { Buttons } from "./components/buttons/buttons.component";
+import { Provider } from "react-redux";
+import { store } from './store/store'
 
 export const AppCore: FunctionComponent = () => {
   return (
-    <div className="app">
-      <Square/>
-      <Buttons/>
-      work
-    </div>
+    <Provider store={store}>
+      <div className="app">
+        <Square/>
+        <Buttons/>
+      </div>
+    </Provider>
   )
 }
