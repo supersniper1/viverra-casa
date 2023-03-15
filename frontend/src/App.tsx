60a0563ce43606ceda27b0e033606efe07bf706a
supersniper1 <denchikden1721@gmail.com>
2023-03-15 22:43:32 +0500

    router added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,23 @@
 import React, {FunctionComponent} from "react";
-import { Square } from "./components/square/square.component";
-import { Buttons } from "./components/buttons/buttons.component";
 import { Provider } from "react-redux";
-import { store } from './store/store'
+import { store } from './store/store';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { View } from "./views/export.views";
+
+const AppRouting: FunctionComponent = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path='/main' element={<View.Main/>}/>
+      <Route path='/test' element={<View.Test/>}/>
+      <Route path='*' element={<View.Error/>}/>
+    </Routes>
+  </BrowserRouter>
+);
 
 export const AppCore: FunctionComponent = () => {
   return (
     <Provider store={store}>
-      <div className="app">
-        <Square/>
-        <Buttons/>
-      </div>
+      <AppRouting/>
     </Provider>
   )
-}
+};
---
+++ frontend/src/views/export.views.ts
@@ -0,0 +1,9 @@
+import {Test} from './test/Test.view';
+import {Main} from './main/Main.view';
+import {Error} from './error/Error.view'
+
+export const View = {
+  Main,
+  Test,
+  Error,
+};
