0a78770a52da0db6af3c787a0ee0c32f901e5c14
supersniper1 <denchikden1721@gmail.com>
2023-03-16 17:17:51 +0500

    changed normalize css to scss

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
+import 'normalize.scss/normalize.scss'
 import { createRoot } from "react-dom/client";
 import { AppCore } from "./App";
-import 'normalize.css';
 
 createRoot(document.getElementById("app") as HTMLElement).render(<AppCore />);
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -39,7 +39,7 @@ const config = {
                 exclude: ['/node_modules/'],
             },
             {
-                test: /\.(s[ac]ss|css)$/i,
+                test: /\.s[ac]ss$/i,
                 use: [stylesHandler, 'css-loader', 'sass-loader'],
             },
             {
