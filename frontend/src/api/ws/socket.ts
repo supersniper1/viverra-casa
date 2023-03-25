d4750cf840979ba113a48a64e84401b4e2cb0df5
supersniper1 <denchikden1721@gmail.com>
2023-03-26 02:31:01 +0500

    websockets test

--- frontend/src/api/ws/socket.ts
+++ frontend/src/api/ws/socket.ts
@@ -1,7 +1,7 @@
 import { io, Socket } from "socket.io-client";
 
 export const socket: Socket = io(
-  process.env.NODE_ENV === "production" ? "/" : "http://localhost:/",
+  process.env.NODE_ENV === "production" ? "ws://backend:8000/ws/socket-server/" : "ws://backend:8000/ws/socket-server/",
   {
     forceNew: true,
     timeout: 1000,
--- frontend/src/views/main/Main.view.tsx
+++ frontend/src/views/main/Main.view.tsx
@@ -1,15 +1,44 @@
-import React, {FunctionComponent} from 'react';
+import React, {FunctionComponent, useEffect, useState} from 'react';
 import {Square} from "@components/square/square.component";
 import {Buttons} from "@components/buttons/buttons.component";
 import {Icons} from "@assets/components/export";
+import {socket} from "@/api/ws/socket";
 
 export const Main: FunctionComponent = () => {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [fooEvents, setFooEvents] = useState([]);
+
+  useEffect(() => {
+    const onConnect = () => {
+      setIsConnected(true);
+    }
+
+    const onDisconnect = () => {
+      setIsConnected(false);
+    }
+
+    const onFooEvent = (value: any) => {
+      setFooEvents(previous => [...previous, value]);
+    }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+    socket.on('foo', onFooEvent);
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+      socket.off('foo', onFooEvent);
+    };
+  }, []);
+
   return (
     <div>
       MainPage
       <Icons.Login/>
       <Square/>
       <Buttons/>
+      {fooEvents}
     </div>
   );
 };
