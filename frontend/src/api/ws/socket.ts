d19b4ca57a9e4a7fe030324436664806f7e21ceb
Костя Бормотов <Bormotov131911@gmail.com>
2023-03-28 00:35:07 +0500

    websockets added

--- frontend/src/api/ws/socket.ts
+++ frontend/src/api/ws/socket.ts
@@ -1,7 +1,7 @@
 import { io, Socket } from "socket.io-client";
 
 export const socket: Socket = io(
-  process.env.NODE_ENV === "production" ? "/test/" : "http://127.0.0.1:8000/test/",
+  process.env.NODE_ENV === "production" ? "/ws/" : "http://127.0.0.1:8000/ws/",
   {
     forceNew: true,
     timeout: 10000,
--- frontend/src/views/main/Main.view.tsx
+++ frontend/src/views/main/Main.view.tsx
@@ -1,53 +1,34 @@
-import React, {FunctionComponent, useEffect, useState} from 'react';
+import React, {FunctionComponent, useEffect} from 'react';
 import {Square} from "@components/square/square.component";
 import {Buttons} from "@components/buttons/buttons.component";
 import {Icons} from "@assets/components/export";
 import {socket} from "@/api/ws/socket";
 
 export const Main: FunctionComponent = () => {
-  const [isConnected, setIsConnected] = useState(socket.connected);
-  const [fooEvents, setFooEvents] = useState([]);
-
   useEffect(() => {
-    const onConnect = () => {
-      setIsConnected(true);
-    }
-
-    const onDisconnect = () => {
-      setIsConnected(false);
-    }
-
-    const onFooEvent = (value: any) => {
-      setFooEvents(previous => [...previous, value]);
-    }
-
-    socket.on('connect', onConnect);
-    socket.on('disconnect', onDisconnect);
-    socket.on('foo', onFooEvent);
-
-    socket.on("connect", () => {
-      console.log("socket connected");
+    socket.on('connect', () => {
+      console.log('connected')
     });
-
-    socket.on("connect_error", (err:any) => {
-      console.log(`connect_error due to ${err.message}`);
-      console.log(`connect_error due to ${err}`);
+    socket.on('disconnect', () => {
+      console.log('disconected')
     });
 
     return () => {
-      socket.off('connect', onConnect);
-      socket.off('disconnect', onDisconnect);
-      socket.off('foo', onFooEvent);
+      socket.off('connect', () => {
+        console.log('connected')
+      });
+      socket.off('disconnect', () => {
+        console.log('disconected')
+      });
     };
   }, []);
 
   return (
     <div>
-      MainPage_Test_v9
+      MainPage
       <Icons.Login/>
       <Square/>
       <Buttons/>
-      {fooEvents}
     </div>
   );
 };
