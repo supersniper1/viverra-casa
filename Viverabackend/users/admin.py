649efdf78d1e3270aa5b2cbc206d7ce615a09cc5
Sovraska <simon.novikov@gmail.com>
2023-04-02 19:07:44 +0300

    BufferUserWidget

--- Viverabackend/users/admin.py
+++ Viverabackend/users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.utils.safestring import mark_safe
+from .models import BufferUserWidgetModel
 
 UserModel = get_user_model()
 
@@ -25,3 +26,13 @@ class UsersAdmin(admin.ModelAdmin):
         return '--пусто--'
 
     avatar_show.short_description = 'аватар'
+
+
+@admin.register(BufferUserWidgetModel)
+class BufferUserWidgetAdmin(admin.ModelAdmin):
+    list_display = (
+        'uuid',
+        'user_uuid',
+        'widget_uuid',
+    )
+
