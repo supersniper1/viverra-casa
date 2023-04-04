8dd02d99455958b498fe90e306b52f4d8e765e15
Sovraska <simon.novikov@gmail.com>
2023-04-04 05:52:17 +0300

    fix asgi

--- Viverabackend/Viverabackend/asgi.py
+++ Viverabackend/Viverabackend/asgi.py
@@ -2,6 +2,13 @@ import os
 
 import socketio
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Viverabackend.settings")
+
+import django
+
+django.setup()
+
 from sockets.routing import sio
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Viverabackend.settings')
